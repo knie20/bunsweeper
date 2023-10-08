5a46ffc80adf924dbc9c325eba9cd741cc4d5d58
knie20 <kdnie20@gmail.com>
2023-10-08 14:49:51 -0400

    dynamic grid generation

--- components/Board.tsx
+++ components/Board.tsx
@@ -14,7 +14,7 @@ export default function Board({boardState}: {
         })
     })
     
-    return <div className="board-grid">
+    return <div className="grid grid-flow-row" style={generateBoardGrid(6, 10)}>
         {tiles}
     </div>
 }
@@ -22,3 +22,9 @@ export default function Board({boardState}: {
 const buildKey = (tile: TileState) => {
     return `${tile.revealed ? 'revealed' : 'unrevealed'}_${tile.mark.toString()}_${tile.value.toString()}`
 };
+
+const generateBoardGrid = (rows: number, columns: number): any => {
+    return {
+        gridTemplate: `repeat(${rows}, 32px) / repeat(${columns}, 32px)`
+    };
+}
